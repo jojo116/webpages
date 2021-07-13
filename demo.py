877864a48a9d80c71d6d5d487e2b4f6ff9cb4d87
Jojo Walugembe <jwalugem@u.rochester.edu>
2021-07-13 21:26:18 +0000

    New Demo File

--- demo.py
+++ demo.py
@@ -20,9 +20,11 @@ class User(db.Model):
     return f"User('{self.username}', '{self.email}')"
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
-    
+
+
 @app.route("/second_page")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
@@ -34,10 +36,8 @@ def register():
       user = User(username=form.username.data, email=form.email.data, password=form.password.data)
       db.session.add(user)
       db.session.commit()   
-    
-    if form.validate_on_submit(): # checks if entries are valid
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home')) # if so - send to home page
+      flash(f'Account created for {form.username.data}!', 'success')
+      return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
   
 if __name__ == '__main__':
