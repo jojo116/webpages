4932be1e88606fc0f77980f368ee51673e57b184
Jojo Walugembe <jwalugem@u.rochester.edu>
2021-07-14 15:40:22 +0000

    File to Run

--- demo.py
+++ demo.py
@@ -4,11 +4,21 @@ from flask import Flask,render_template,url_for
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+from recog import printWAV
+import time, random, threading
+from turbo_flask import Turbo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0b2163b0200a1eda34b0c56fab9b168f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+
+interval=10
+FILE_NAME = "willsmith.wav"
+turbo = Turbo(app)
+
+
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -25,7 +35,7 @@ def home():
     return render_template('home.html', subtitle='Home Page', text='This is the home page')
 
 
-@app.route("/second_page")
+@app.route("/about")
 def second_page():
     return render_template('second_page.html', subtitle='Second Page', text='This is the second page')
 
@@ -39,6 +49,48 @@ def register():
       flash(f'Account created for {form.username.data}!', 'success')
       return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
+ 
+
+@app.route("/captions")
+def captions():
+    TITLE = "Will Smith Motivation"
+    FILE_NAME = "willsmith.wav"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions, daemon=True).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+
   
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+  app.run(debug=True, host="0.0.0.0")
